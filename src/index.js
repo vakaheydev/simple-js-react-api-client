79153f8313eddfaf281e6c6e91488ee13b3f284a
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-04-10 16:12:45 +0300

    Refactoring to gh pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router";
+import { createHashRouter, RouterProvider } from "react-router";
+
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <App/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
